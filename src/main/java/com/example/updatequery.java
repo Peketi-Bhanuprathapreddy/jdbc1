bcd21627a092c76aab013a0ff98c3d4180b22cdf
Bhanu prathap reddy <104185891+Peketi-Bhanuprathapreddy@users.noreply.github.com>
2022-05-12 18:06:20 +0530

    commit

---
+++ src/main/java/com/example/updatequery.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+ 
+public class updatequery {
+     public static void main(String[] args) {
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver"); // Register the Driver
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bhanu", "root", "root");
+
+        String query = "update emp set Name=?,Role=? where Branch=? ";
+        BufferedReader br=new BufferedReader( new InputStreamReader(System.in));
+        System.out.println("enter the name: ");
+        String Name=br.readLine();
+        System.out.println("enter the roll: ");
+        String Role=br.readLine();
+        System.out.println("enter the branch: ");
+        String Branch=br.readLine();
+        PreparedStatement ps = con.prepareStatement(query);
+
+        ps.setString(1, Name);
+ps.setString(2, Role);
+ps.setString(3, Branch);
+
+
+     
+       ps.execute(); 
+con.close();
+    } catch(Exception se) {
+        System.out.println(se.getMessage());
+    }
+}
+}
+    
+
